ec00719522d19876d82cbd9ed19a216e941224ca
eng24am0217-rgb <eng24am0217@dsu.edu.in>
2025-12-01 18:42:45 +0530

    Create array4.java

---
+++ array4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class array4{
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        int n;
+        System.out.println("Enter the size:");
+        n=s.nextInt();
+        System.out.println("Enter the array elements:");
+        int a[]=new int[n],i;
+        for(i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        int tar;
+        System.out.println("Enter the number:");
+        tar=s.nextInt();
+        for(i=0;i<n;i++){
+            if(tar==a[i]) System.out.println("Element "+ tar+" is present in the index "+i);
+            if(i==n-1){
+                if(tar!=a[i]) System.out.println("Element not found!");
+            }
+        }
+        
+    }
+}
